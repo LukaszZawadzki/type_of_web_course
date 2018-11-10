65df70dbdbee38d4696f1768d3806131dca511cf
Łukasz Zawadzki <l.zawadzki@vp.pl>
2018-11-10 08:01:27 +0000

    Updates

---
+++ precache-manifest.7f0fcaf6ee5d302d5ffc0daf7f2d2d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/type_of_web_course/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed0de4280edc27c4379e",
+    "url": "/type_of_web_course/static/js/runtime~main.ed0de428.js"
+  },
+  {
+    "revision": "bb10efcbbd10c2988a50",
+    "url": "/type_of_web_course/static/js/main.bb10efcb.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/type_of_web_course/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "bb10efcbbd10c2988a50",
+    "url": "/type_of_web_course/static/css/main.81a37ce8.chunk.css"
+  },
+  {
+    "revision": "6fd1250f2d3e52dfcd6bf2f2c4be254f",
+    "url": "/type_of_web_course/index.html"
+  }
+];
